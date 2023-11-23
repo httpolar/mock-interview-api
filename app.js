61f9bda38f877aa7a736da47bab90632c3bc295c
Kirill M. <96192210+httpolar@users.noreply.github.com>
2023-11-23 22:13:18 +0400

    fix missing cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var packagesRouter = require("./routes/packages");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
